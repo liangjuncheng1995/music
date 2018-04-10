c4672d35cc821c4a94e5daca35ba44e44fea8eca
cheng <18819206582@163.com>
2018-04-10 09:41:57 +0800

    4.10

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -5,7 +5,7 @@
         <h2 class="list-group-title">{{group.title}}</h2>
         <ul>
           <li v-for="item in group.items" class="list-group-item">
-            <img :src="item.avatar" class="avatar" alt="">
+            <img v-lazy="item.avatar" class="avatar" alt="">
             <span class="name">{{item.name}}</span>
           </li>
         </ul>
@@ -64,30 +64,5 @@
   color: rgba(255, 255, 255, 0.5);
   font-size: 14px;
 }
-  /* position: relative
-  width: 100%
-  height: 100%
-  overflow: hidden
-  background: $color-background
-  .list-group
-    padding-bottom: 30px
-    .list-group-title
-      height: 30px
-      line-height: 30px
-      padding-left: 20px
-      font-size: $font-size-small
-      color: $color-text-l
-      background: $color-highlight-background
-    .list-group-item
-      display: flex
-      align-items: center
-      padding: 20px 0 0 30px
-      .avatar
-        width: 50px
-        height: 50px
-        border-radius: 50%
-      .name
-        margin-left: 20px
-        color: $color-text-l
-        font-size: $font-size-medium */
+
 </style>
